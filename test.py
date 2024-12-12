93b52e45d9a515e876bce1ee0b92ab297bbfa616
y701311 <yuukiyamada2005@gmail.com>
2024-12-12 17:08:48 +0900

    モデルのテストコード

---
+++ test.py
@@ -0,0 +1,93 @@
+from __future__ import print_function
+
+import argparse
+import os
+import time
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+from helpers.loaders import *
+from helpers.utils import adjust_learning_rate
+from models import ResNet18
+from trainer import test, train, trigger_subsets_test
+
+def main():
+    parser = argparse.ArgumentParser(description='Train CIFAR-10 models with watermaks.')
+    parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+    parser.add_argument('--train_db_path', default='./data', help='the path to the root folder of the traininng data')
+    parser.add_argument('--test_db_path', default='./data', help='the path to the root folder of the traininng data')
+    parser.add_argument('--dataset', default='cifar10', help='the dataset to train on [cifar10]')
+    parser.add_argument('--wm_path', default='./data/trigger_set/', help='the path the wm set')
+    parser.add_argument('--wm_lbl', default='labels-cifar.txt', help='the path the wm random labels')
+    parser.add_argument('--batch_size', default=100, type=int, help='the batch size')
+    parser.add_argument('--wm_batch_size', default=2, type=int, help='the wm batch size')
+    parser.add_argument('--max_epochs', default=60, type=int, help='the maximum number of epochs')
+    parser.add_argument('--lradj', default=20, type=int, help='multiple the lr by 0.1 every n epochs')
+    parser.add_argument('--save_dir', default='./checkpoint/', help='the path to the model dir')
+    parser.add_argument('--save_model', default='model.t7', help='model name')
+    parser.add_argument('--load_path', default='./checkpoint/ckpt.t7', help='the path to the pre-trained model, to be used with resume flag')
+    parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+    parser.add_argument('--wmtrain', '-wmt', action='store_true', help='train with wms?')
+    parser.add_argument('--log_dir', default='./log', help='the path the log dir')
+    parser.add_argument('--runname', default='train', help='the exp name')
+
+    args = parser.parse_args()
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+    LOG_DIR = args.log_dir
+    if not os.path.isdir(LOG_DIR):
+        os.mkdir(LOG_DIR)
+    logfile = os.path.join(LOG_DIR, 'log_' + str(args.runname) + '.txt')
+    confgfile = os.path.join(LOG_DIR, 'conf_' + str(args.runname) + '.txt')
+
+    # save configuration parameters
+    with open(confgfile, 'w') as f:
+        for arg in vars(args):
+            f.write('{}: {}\n'.format(arg, getattr(args, arg)))
+
+    trainloader, testloader, n_classes = getdataloader(
+        args.dataset, args.train_db_path, args.test_db_path, args.batch_size)
+
+    print('Loading watermark images')
+    wmloader, wmloader_noshuffle = getwmloader(args.wm_path, args.wm_batch_size, args.wm_lbl)
+
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.exists(args.load_path), 'Error: no checkpoint found!'
+    checkpoint = torch.load(args.load_path)
+    net = checkpoint['net']
+    acc = checkpoint['acc']
+    start_epoch = checkpoint['epoch']
+
+    net = net.to(device)
+    # support cuda
+    if device == 'cuda':
+        print('Using CUDA')
+        print('Parallel training on {0} GPUs.'.format(torch.cuda.device_count()))
+        net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
+        cudnn.benchmark = True
+
+    # criterion = nn.CrossEntropyLoss()
+    criterion = nn.KLDivLoss()
+    # criterion = nn.KLDivLoss(reduction="batchmean") # 確率分布を正解として損失を計算する
+    # criterion = nn.MSELoss()
+    
+    print("Test acc:")
+    test_loss, test_acc = test(net, criterion, logfile, testloader, device)
+    print("WM acc:")
+    wm_loss, wm_acc = test(net, criterion, logfile, wmloader, device)
+    print("WM set acc:")
+    wm_subset_acc = trigger_subsets_test(net, criterion, logfile, wmloader_noshuffle, device, args.wm_path, args.wm_lbl, subset_size=5)
+
+
+if __name__ == '__main__':
+    from multiprocessing import freeze_support
+    freeze_support()
+    main()
